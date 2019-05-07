ed9beb509e87eef6a0e1b7699f8ddd948df9a73d
jihun4170 <thrp4170@naver.com>
2019-05-08 05:33:12 +0900

    ss

--- app/src/main/java/com/example/activity_maim/Funding_list_adapter.java
+++ app/src/main/java/com/example/activity_maim/Funding_list_adapter.java
@@ -1,16 +1,104 @@
 package com.example.activity_maim;
 
-import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class Funding_list_adapter extends RecyclerView.Adapter<Funding_list_adapter.ViewHolder> {
+public class Funding_list_adapter extends RecyclerView.Adapter<Funding_list_adapter.ItemViewHolder> {
+
+    // adapter에 들어갈 list 입니다.
+    private ArrayList<Data> listData = new ArrayList<>();
+    private Funding_list_adapter.Callback callback;
+
+    public Funding_list_adapter(Funding_list_adapter.Callback callback) {
+        this.callback = callback;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+
+
+        // LayoutInflater를 이용하여 전 단계에서 만들었던 item.xml을 inflate 시킵니다.
+        // return 인자는 ViewHolder 입니다.
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ItemViewHolder holder, int position) {
+        // Item을 하나, 하나 보여주는(bind 되는) 함수입니다.
+        holder.getLinearLayout().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callback.onCallback();
+            }
+        });
+        holder.onBind(listData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        // RecyclerView의 총 개수 입니다.
+        return listData.size();
+    }
+
+    void addItem(Data data) {
+        // 외부에서 item을 추가시킬 함수입니다.
+        listData.add(data);
+    }
+
+    public interface Callback {
+        public void onCallback();
+    }
+
+    // RecyclerView의 핵심인 ViewHolder 입니다.
+    // 여기서 subView를 setting 해줍니다.
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textView1;
+        private TextView textView2;
+        private ImageView imageView;
+
+        public LinearLayout getLinearLayout() {
+            return linearLayout;
+        }
+
+        private LinearLayout linearLayout;
+
+        ItemViewHolder(View itemView) {
+            super(itemView);
+
+            textView1 = itemView.findViewById(R.id.textView1);
+            textView2 = itemView.findViewById(R.id.textView2);
+            imageView = itemView.findViewById(R.id.imageView);
+            linearLayout = itemView.findViewById(R.id.list_item_layout);
+        }
+
+        void onBind(Data data) {
+            textView1.setText(data.getTitle());
+            textView2.setText(data.getContent());
+            imageView.setImageResource(data.getResId());
+
+//            itemView.setOnClickListener((View.OnClickListener) this);
+//            textView1.setOnClickListener((View.OnClickListener) this);
+//            textView2.setOnClickListener((View.OnClickListener) this);
+//            imageView.setOnClickListener((View.OnClickListener) this);
+        }
+    }
+}
+
+/* public class Funding_list_adapter extends RecyclerView.Adapter<Funding_list_adapter.ViewHolder> {
     private final LayoutInflater inflater;
 
     ArrayList<String> _title;
@@ -47,3 +135,4 @@ public class Funding_list_adapter extends RecyclerView.Adapter<Funding_list_adap
         }
     }
 }
+*/
--- app/src/main/java/com/example/activity_maim/fundlist.java
+++ app/src/main/java/com/example/activity_maim/fundlist.java
@@ -1,6 +1,7 @@
 package com.example.activity_maim;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -9,25 +10,131 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+
 
 public class fundlist extends AppCompatActivity {
+
+    private Funding_list_adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fundlist);
+
+        View.OnClickListener add1 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addIntent = new Intent(fundlist.this, fundin.class);
+                fundlist.this.startActivity(addIntent);
+            }
+        };
+        FloatingActionButton floatingActionButton = findViewById(R.id.add1);
+        floatingActionButton.setOnClickListener(add1);
+        init();
+        getData();
+    }
+
+
+    private void init() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter = new Funding_list_adapter(new Funding_list_adapter.Callback() {
+
+            @Override
+            public void onCallback() {
+                Intent itemIntent = new Intent(fundlist.this, fundingmain.class);
+                startActivity(itemIntent);
+            }
+        });
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void getData() {
+        // 임의의 데이터입니다.
+        List<String> listTitle = Arrays.asList("" +
+                "RunRace3D",
+                "TinkerIsland",
+                "NonStop Knight",
+                "스도쿠",
+                "Racing Moto",
+                "쿠킹다이어리",
+                "Real Piano",
+                "포커"
+
+        );
+        List<String> listContent = Arrays.asList(
+                "스포츠",
+                "어드벤처",
+                "롤플레잉",
+                "퍼즐",
+                "레이싱",
+                "시뮬레이션",
+                "음악",
+                "카드"
+
+        );
+        List<Integer> listResId = Arrays.asList(
+                R.drawable.l1,
+                R.drawable.l2,
+                R.drawable.l3,
+                R.drawable.l4,
+                R.drawable.l5,
+                R.drawable.l6,
+                R.drawable.l7,
+                R.drawable.l8
+
+        );
+        for (int i = 0; i < listTitle.size(); i++) {
+            // 각 List의 값들을 data 객체에 set 해줍니다.
+            Data data = new Data();
+            data.setTitle(listTitle.get(i));
+            data.setContent(listContent.get(i));
+            data.setResId(listResId.get(i));
+
+            // 각 값이 들어간 data를 adapter에 추가합니다.
+            adapter.addItem(data);
+        }
+
+        // adapter의 값이 변경되었다는 것을 알려줍니다.
+        adapter.notifyDataSetChanged();
+    }
+    }
+/*public class fundlist<linearLayoutManager> extends AppCompatActivity {
     private ArrayList<HashMap<String,String>> Data = new ArrayList<HashMap<String, String>>();
     private HashMap<String,String> InputData1 = new HashMap<>();
    private HashMap<String,String> InputData2 = new HashMap<>();
    private ListView listView;
    private ArrayList<String> title;
+
+
+
   @Override
+
+
+
   protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_fundlist);
       title=new ArrayList<>();
       for(int i=0;i<6;i++){
-          title.add("손이 절로가네 게임");
+          title.add("손이 절로가네 게임1");
+          title.add("손이 절로가네 게임2");
+
+          title.add("손이 절로가네 게임3");title.add("손이 절로가네 게임3");
+          title.add("손이 절로가네 게임3");title.add("손이 절로가네 게임3");
+          title.add("손이 절로가네 게임3");title.add("손이 절로가네 게임3");
+          title.add("손이 절로가네 게임3");title.add("손이 절로가네 게임3");
       }
       Funding_list_adapter adapter=new Funding_list_adapter(fundlist.this,title);
 
@@ -37,3 +144,4 @@ public class fundlist extends AppCompatActivity {
 
   }
 }
+*/

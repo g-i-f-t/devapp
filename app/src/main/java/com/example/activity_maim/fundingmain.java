a0c9ec082f5797bc878aedf9bdd3ba43a189c52e
jihun4170 <thrp4170@naver.com>
2019-06-11 05:33:39 +0900

    지훈ㄱ캐리

--- app/src/main/java/com/example/activity_maim/fundingmain.java
+++ app/src/main/java/com/example/activity_maim/fundingmain.java
@@ -1,18 +1,43 @@
 package com.example.activity_maim;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ProgressBar;
+
+import com.akexorcist.roundcornerprogressbar.IconRoundCornerProgressBar;
+import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
+
 
 public class fundingmain extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fundingmain);
 
-        ProgressBar progressBar = (ProgressBar) findViewById(R.id.fundprogress);
-        progressBar.setMax(100);
-        progressBar.setProgress(45);
+        RoundCornerProgressBar progress1 = (RoundCornerProgressBar) findViewById(R.id.progress_1);
+        progress1.setProgressBackgroundColor(Color.parseColor("#ddd9da"));
+        progress1.setProgressColor(Color.parseColor("#ffe100"));
+
+
+        progress1.setMax(100);
+        progress1.setProgress(80);
+
+        if (progress1.getProgress() < 25){
+            progress1.setProgressColor(Color.parseColor("#ffe100"));
+        }else if(progress1.getProgress() < 50) {
+            progress1.setProgressColor(Color.parseColor("#0400ff"));
+        }
+        else if(progress1.getProgress() < 75) {
+            progress1.setProgressColor(Color.parseColor("#d81b60"));
+        }
+        else {
+            System.out.println("10000000000000000000"+progress1.getProgress());
+            progress1.setProgressColor(Color.parseColor("#1ed81b"));
+        }
     }
 }
--- app/src/main/java/com/example/activity_maim/fundingmain1.java
+++ app/src/main/java/com/example/activity_maim/fundingmain1.java
@@ -2,10 +2,14 @@ package com.example.activity_maim;
 
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
 
 public class fundingmain1 extends AppCompatActivity {
 
@@ -15,9 +19,11 @@ public class fundingmain1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fundingmain1);
 
-        ProgressBar progressBar = (ProgressBar) findViewById(R.id.fundprogress);
-        progressBar.setMax(100);
-        progressBar.setProgress(100);
+        RoundCornerProgressBar progress2 = (RoundCornerProgressBar) findViewById(R.id.progress_2);
+        progress2.setProgressColor(Color.parseColor("#d81b60"));
+        progress2.setProgressBackgroundColor(Color.parseColor("#ddd9da"));
+        progress2.setMax(100);
+        progress2.setProgress(100);
 
         withdraw =  findViewById(R.id.wd);
 
